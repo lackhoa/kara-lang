166c83e4ab676647a4775e04084557e502eeb3c7
lackhoa <vodangkhoa873@gmail.com>
2018-09-10 03:18:58 +0300

    set!ret

--- macro.rkt
+++ macro.rkt
@@ -2,42 +2,51 @@
 (provide (all-defined-out))
 
 
-; "lam" instead of "lambda"
+;; "lam" instead of "lambda"
 (define-syntax-rule (lam whatever ...)
   (lambda whatever ...))
 
-; "def" instead of "define"
+;; "def" instead of "define"
 (define-syntax-rule (def whatever ...)
   (define whatever ...))
 
-; "def*" instead of "define-values"
+;; "def*" instead of "define-values"
 (define-syntax-rule (def* (id ids ...) expr)
   (match-define (list id ids ...) expr))
 
-; Switch: an even simpler dispatch than `case`
+;; Switch: an even simpler dispatch than `case`
 (define-syntax switch
   (syntax-rules (else)
-    ; Else
+    ;; Else
     [(_ val [else e1 e2 ...]) (begin e1 e2 ...)]
-    ; No cases left
+    ;; No cases left
     [(_ val) (error "Switch" "No match found" val)]
-    ; Cases left
+    ;; Cases left
     [(_ val [compare e1 e2 ...] rest ...)
      (if (eq? val compare)
          (begin e1 e2 ...)
-       (switch val rest ...))]))
+         (switch val rest ...))]))
 
-; Add an element to the beginning of the list.
 (define-syntax-rule (cons! item ls)
+  ;; Add an element to the beginning of the list.
   (set! ls (cons item
                  ls)))
 
-; Same, but with streams
 (define-syntax-rule (stream-cons! item stream)
+  ;; Same, but with streams
   (set! stream (stream-cons item
                             stream)))
 
-; Same idea, but with append
 (define-syntax-rule (append! mutated-ls ls)
+  ;; Same idea, but with append
   (set! mutated-ls
-    (append mutated-ls ls)))
+        (append mutated-ls ls)))
+
+(define-syntax-rule (cdr! ls)
+  ;; I'm just gonna do it without saying a thing
+  (set! ls (cdr ls)))
+
+(define-syntax-rule (set!ret var val)
+  ;; Set and then return the value
+  (begin (set! var val)
+         var))

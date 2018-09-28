1f4b5ec30805bcfccd7e6b2d8d8043556345fb2c
lackhoa <vodangkhoa873@gmail.com>
2018-09-28 18:35:13 +0300

    Move everything to a single file

--- kara.rkt
+++ kara.rkt
@@ -1,17 +1,216 @@
 #lang racket
+(require racket/trace
+         racket/generator
+         rackunit)
+(provide (all-from-out racket/trace)
+         (all-from-out racket/generator)
+         (all-defined-out))
 
-(require "macro.rkt"
-         "utils.rkt"
-         "seq.rkt"
-         "stack.rkt"
-         "set.rkt"
-         racket/trace
-         racket/generator)
+;;; Macros
+(define-syntax-rule (lam whatever ...)
+  (lambda whatever ...))
 
-(provide (all-from-out "macro.rkt")
-         (all-from-out "utils.rkt")
-         (all-from-out "seq.rkt")
-         (all-from-out "stack.rkt")
-         (all-from-out "set.rkt")
-         (all-from-out racket/trace)
-         (all-from-out racket/generator))
+(define-syntax-rule (def whatever ...)
+  (define whatever ...))
+
+(define-syntax-rule (def* (id ids ...) expr)
+  (match-define (list id ids ...) expr))
+
+(define-syntax-rule (cons! item ls)
+  (set! ls (cons item
+                 ls)))
+
+(define-syntax-rule (stream-cons! item stream)
+  ;; Same, but with streams
+  (set! stream (stream-cons item
+                            stream)))
+
+(define-syntax-rule (append! mutated-ls ls)
+  (set! mutated-ls
+    (append mutated-ls ls)))
+
+(define-syntax-rule (cdr! ls)
+  (set! ls (cdr ls)))
+
+(define-syntax-rule (assert bool msg objs ...)
+  (unless bool (error msg objs ...)))
+
+(define-syntax-rule (orb e ...)
+  (bool (or e ...)))
+
+(define-syntax-rule (andb e ...)
+  (bool (and e ...)))
+
+;;; Functional Stuff
+(def (repeat times func)
+  (when (> times 0)
+    (func)
+    (repeat (sub1 times) func)))
+
+(def (nequal? x y)
+  (not (equal? x y)))
+
+(def (neq? x y)
+  (not (eq? x y)))
+
+(def (eq*? . args)
+  (andmap (lam (x)  (eq? x (car args)))
+          (cdr args)))
+
+(def (equal*? . args)
+  (andmap (lam (x)  (equal? x (car args)))
+          (cdr args)))
+
+(def (bool x)
+  (match x [#f  #f] [_   #t]))
+
+(def (flip fn)
+  (lam (x y) (fn y x)))
+
+(def (sum-list ls)
+  (foldr + 0 ls))
+
+(def (best fn ls)
+  (match ls
+    [(list)          'no-elem]
+    [(cons fst rst)  (let ([winner fst])
+                       (for ([obj rst]) (when (fn obj winner)
+                                          (set! winner obj)))
+                       winner)]))
+
+(def (most fn ls)
+  (match ls
+    [(list)          'no-elem]
+    [(cons fst rst)  (let ([res (list fst)]
+                           [max (fn fst)])
+                       (for ([obj rst])
+                         (let ([score (fn obj)])
+                           (cond [(> score max)
+                                  (set! res (list obj))
+                                  (set! max score)]
+                                 [(= score max)
+                                  (cons! obj res)]))))]))
+
+;;; Hash Tables
+(def (hash-set-many ht ls val)
+  (for ([item ls])
+    (set! ht (hash-set ht item val)))
+  ht)
+
+(define-syntax-rule (def-mem (f args ...) bodies ...)
+  ;; replace define with a memoized version
+  (define f
+    ;; store the cache as a hash of args => result
+    (let ([results (make-hash)])
+      (lambda (args ...)
+        (hash-ref! results
+                   (list args ...)
+                   (thunk (begin bodies ...)))))))
+
+;;; Testing Functions
+(def (fib n)
+  (cond [(= 0 n) 1]
+        [(= 1 n) 1]
+        [else (+ (fib (- n 1)) (fib (- n 2)))]))
+
+(def (square n)
+  (* n n))
+
+;;; Assertions
+(define-simple-check (check-class obj class)
+  (is-a? obj class))
+
+;;; Display
+(def (pdisplay x
+               [columns 35]
+               [port (current-output-port)])
+  (parameterize ([pretty-print-columns columns])
+    (pretty-display x port)))
+
+(def (pydisplay . objs)
+  (for ([obj objs])
+    (display obj)
+    (display " "))
+  (newline))
+
+;;; Generators
+(def (gen-get gen num
+              [func (lam (x) (pdisplay x) (newline))])
+  (match num
+    [0              (void)]
+    [(? positive?)  (match (gen)
+                      ['DONE (void)]
+                      [val   (func val)
+                             (gen-get gen
+                                      (sub1 num)
+                                      func)])]
+    [_              (error "Invalid number" num)]))
+
+(define (gen->list gen num)
+  ;; Returns: a list
+  (match num
+    [0              null]
+    [(? positive?)  (match (gen)
+                      ['DONE  null]
+                      [val    (cons val
+                                    (gen->list gen (sub1 num)))])]
+    [_              (error "Invalid number" num)]))
+
+;; The impersonator pattern: yield all
+;; values that `gen` yields.
+(define-syntax-rule (gen-impersonate gen)
+  ;; `g` stops the generator from being re-defined every loop
+  (let ([g gen])
+    (let loop ()
+      (match (g)
+        ['DONE 'DONE]
+        [any
+         (begin (yield any)
+                (loop))]))))
+
+;;; Sequence
+(def (stream-member x seq)
+  (cond [(stream-empty? seq)   #f]
+        [(equal? x (car seq))  seq]
+        [else                  (stream-member x (cdr seq))]))
+
+(def (forall? pred seq)
+  (bool (findf (negate pred) seq)))
+
+(def (last-index ls)
+  (sub1 (length ls)))
+
+(def (stream-remove x s)
+  (stream-filter (lam (item)
+                   (not (equal? item x)))
+                 s))
+
+(define (flatmap func ls)
+  (foldr append null (map func ls)))
+
+(def (drop-pos ls pos)
+  (let-values ([(left right)  (split-at ls pos)])
+    (append left (cdr right))))
+
+;; Add an item to the end of a list
+(def (pad ls single-item)
+  (append ls (list single-item)))
+
+;;; The stack
+(define stack%
+  (class object%
+    (define content null)
+    (super-new)
+
+    (define/public (push item)
+      (set! content (cons item content)))
+
+    (define/public (pop)
+      (when (null? content)
+        (error "pop!" "Cannot pop an empty stack!" this))
+      (let ([popped-item (car content)])
+        (set! content (cdr content))
+        popped-item))
+
+    (define/public (peek)
+      (car content))))
